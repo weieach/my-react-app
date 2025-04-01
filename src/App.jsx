d001cadbe8d99f3291d73ffdeda062e012c80efb
Nicole Wei <weiyichen2004@gmail.com>
2025-04-01 16:34:07 -0500

    set up board component with 9 grids

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import './App.css';
+import "./App.css";
 
 export default function App() {
-  return(
+  return (
     <>
-    <Square />
+      <Board />
     </>
-  )
-  
+  );
 }
 
-export function Square() {
-  return <button className="square">X</button>;
+export function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+    </>
+  );
 }
