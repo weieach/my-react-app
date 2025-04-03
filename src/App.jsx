360ec5475d2ffbe1d2374327c28252f65bae2e5c
Nicole Wei <weiyichen2004@gmail.com>
2025-04-03 14:47:05 -0500

    added useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
   return (
@@ -12,25 +13,21 @@ export function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value = "1"/>
-        <Square value = "2"/>
-        <Square value = "3"/>
+        <Square />
+        <Square /><Square />
       </div>
       <div className="board-row">
-        <Square value = "4"/>
-        <Square value = "5"/>
-        <Square value = "6"/>
+      <Square /><Square /><Square />
       </div>
       <div className="board-row">
-        <Square value = "7"/>
-        <Square value = "8"/>
-        <Square value = "9"/>
+      <Square /><Square /><Square />
       </div>
     </>
   );
 }
 
 export function Square({ value }) {
+  const [value, setValue] = useState(null);
   function handleClick() {
     console.log('clicked');
   }
